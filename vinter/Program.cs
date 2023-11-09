818ee109ed53fdfc395373d1a2a69995de8b8d21
David Öjebrink Olssson <David.ojebrinkolsson@elev.ga.ntig.se>
2023-11-09 11:02:27 +0100

    first commit

---
+++ vinter/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// game ide quest game diffrent weapons and diffrent missions
+
+// choose name 
+// if name is longer then 6 digits say you have very bad name
+
+
+
+//  Writline hello and wlecome to the wolrld of the old Nti 
+// You will have to survive and interact to pepole in order to get the matkort
+
+
+// hello (name)
